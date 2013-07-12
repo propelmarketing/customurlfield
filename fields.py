4fcc51df3bcf32ffaef0a667147a3d02764499a0
Tyrel Souza <tyrel@tyrelsouza.com>
2013-07-12 11:06:20 -0400

    Added custom upload_to, need to do delete and i'm good

--- fields.py
+++ fields.py
@@ -1,149 +1,31 @@
 #-*- coding: utf-8 -*-
-
-from django import forms
 from django.db import models
-from django.forms import fields
+import hashlib
 
 
-"""
-full_url = URLField(default_protocol="http",
-           protocols=["https","ssh","mailto"])
-url = URLField()
-"""
+class S3FileField(models.FileField):
+    description = "S3 File Field"
 
+    def __init__(self, s3_fields=[], *args, **kwargs):
+        """
+        Setup the S3FileField with parameters and a max_length
+        """
+        self.s3_fields = s3_fields
+        kwargs['upload_to'] = self.upload_to
 
-class URLField(models.Field):
-    """
-    Custom Django URLField that takes acceptable protocols.
-    """
-    description = "URL Field"
-    __metaclass__ = models.SubfieldBase
+        super(S3FileField, self).__init__(*args, **kwargs)
 
-    def __init__(self, default_protocol="http", protocols=[], *args, **kwargs):
-        """
-        Setup the URLField with parameters and a max_length
-        """
-        #Sets the max length of 4096 in the database.
-        kwargs['max_length'] = kwargs.get("max_length", 4096)
-        #Sets the default protocol
-        self.default_protocol = default_protocol
-        #Makes sure protocols is a list, as I insert to the list later.
-        if type(protocols) is list:
-            self.protocols = protocols
-        else:
-            raise ValueError("protocols must be a list")
-        super(URLField, self).__init__(*args, **kwargs)
+    def upload_to(self, instance, filename):
+        path_fields = []
+        for attr in self.s3_fields:
+            path_fields.append(getattr(instance, attr))
 
-    def to_python(self, value):
-        """
-        Either return a blank string, or the url.
-        """
-        if not value:
-            return ""
-        elif isinstance(value, basestring):
-            return value
+        md5 = hashlib.md5()
+        for chunk in iter(lambda: getattr(instance, self.name).file.read(8192), b''):   # NEED TO FIX THIS ATTR
+            md5.update(chunk.encode('utf-8'))
+        file_hash = md5.hexdigest()
 
-    def db_type(self, connection):
-        """
-        Set as VARCHAR for Postgres.
-        """
-        #TODO: Set up multiple backend testing.
-        return "VARCHAR(%s) NOT NULL DEFAULT ''::character varying" % \
-            (self.max_length, )
-
-    def formfield(self, **kwargs):
-        """
-        Setup to use the URLFieldForm for our fields form.
-        """
-        defaults = {'form_class': URLFieldForm}
-        defaults['default_protocol'] = self.default_protocol
-        defaults['protocols'] = self.protocols
-        defaults.update(kwargs)
-        return super(URLField, self).formfield(**defaults)
-
-    def get_prep_value(self, value):
-        """
-        This will check whether the url is either:
-        Protocolless - (and appends a default_protocol to the beginning)
-        has a correct protoco - (returns the url)
-        If neither of these are a result, it will error with an appropriate
-            error message
-        """
-        if not value:
-            return None
-        else:
-            url_parts = str(value).split("://")
-            if len(url_parts) == 1:
-                value = "%s://%s" % (self.default_protocol, value)
-                return value
-            elif len(url_parts) == 2:
-                if url_parts[0].lower() in self.protocols or\
-                   url_parts[0].lower() == self.default_protocol:
-                    if url_parts[1]:
-                        return value
-                    else:
-                        raise forms.ValidationError(
-                            "Must supply more than just a protocol.")
-                else:
-                    acceptable = self.protocols
-                    acceptable.insert(0, self.default_protocol)
-                    raise forms.ValidationError(
-                        "Protocol not accepted. (%s) MUST BE %s" %
-                        (url_parts[0], acceptable))
-            else:
-                raise forms.ValidationError("Not a well formatted URL")
-
-
-class URLFieldForm(fields.CharField):
-    """
-    Custom Django URLFieldForm
-    """
-    def __init__(self, *args, **kwargs):
-        """
-        Setup the URLField with parameters and a max_length
-        """
-        self.default_protocol = kwargs.pop("default_protocol", "http")
-        self.protocols = kwargs.pop("protocols", [])
-
-        dwargs = {
-            'required': False, 'label': None, 'blank': True, 'initial': None,
-            'help_text': None, 'error_messages': None,
-            'show_hidden_initial': None,
-        }
-        for attr in dwargs:
-            if attr in kwargs:
-                dwargs[attr] = kwargs[attr]
-
-        super(URLFieldForm, self).__init__(*args, **kwargs)
-
-    def clean(self, value):
-        """
-        This will check whether the url is either:
-        Protocolless - (and appends a default_protocol to the beginning)
-        has a correct protoco - (returns the url)
-        If neither of these are a result, it will error with an appropriate
-            error message
-        """
-        if not value:
-            return None
-        else:
-            url_parts = value.split("://")
-            if len(url_parts) == 1:
-                value = "%s://%s" % (self.default_protocol, value)
-                return value
-            elif len(url_parts) == 2:
-                if url_parts[0].lower() in self.protocols or\
-                   url_parts[0].lower() == self.default_protocol:
-                    if url_parts[1]:
-                        return value
-                    else:
-                        raise forms.ValidationError(
-                            "Must supply more than just a protocol.")
-                else:
-                    acceptable = self.protocols
-                    acceptable.insert(0, self.default_protocol)
-                    raise forms.ValidationError(
-                        "Protocol not accepted. (%s) MUST BE %s" %
-                        (url_parts[0], acceptable))
-            else:
-                raise forms.ValidationError("Not acceptable URL")
+        path_fields.extend([filename.split(".")[-1],
+                            file_hash,
+                            filename])
+        return "/".join(path_fields)
